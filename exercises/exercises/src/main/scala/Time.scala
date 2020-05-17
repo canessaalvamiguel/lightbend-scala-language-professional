b82d584a77837533afd0da571f4f7b4b5ffae87c
canessaalvamiguel <canessaalvamiguel@gmail.com>
2020-05-17 00:32:51 -0500

    Exercise: Define a Field

--- exercises/exercises/src/main/scala/Time.scala
+++ exercises/exercises/src/main/scala/Time.scala
@@ -1,4 +1,5 @@
 class Time(val hours: Int, val minutes: Int){
+    val asMinutes: Int= hours * 60 + minutes
     //TODO: Verify that hours is within 0 and 23
     //TODO: comment: Verify that minutes is within 0 and 59
 }
